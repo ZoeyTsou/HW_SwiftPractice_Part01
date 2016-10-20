17df1596f14e9c0af3f698bdc0320470281460a6
Zoey <Zoey@Zoey-MacBook-Pro.local>
2016-10-20 17:33:04 +0800

    Initial Commit

---
+++ HW_swiftPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW_swiftPractice
+//
+//  Created by Zoey on 2016/10/20.
+//  Copyright © 2016年 Zoey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
